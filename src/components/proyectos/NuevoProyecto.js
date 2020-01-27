568ab7df2780b9d6b799832355c97f1515a5a194
Jimmy <bruno_pier@hotmail.com>
2020-01-27 19:16:25 -0300

    Ternary code to show form - NuevoProyecto.js

--- src/components/proyectos/NuevoProyecto.js
+++ src/components/proyectos/NuevoProyecto.js
@@ -1,6 +1,11 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useContext } from 'react';
+import proyectoContext from '../../context/proyectos/proyectoContext';
 
 const NuevoProyecto = () => {
+
+    //Obtener state formulario
+    const proyectosContext = useContext(proyectoContext);
+    const { formulario } = proyectosContext;
 
     //State para proyecto
     const [proyecto, guardarProyecto] = useState({
@@ -37,26 +42,31 @@ const NuevoProyecto = () => {
                 className="btn btn-block btn-primario"
             >Nuevo Proyecto</button>
 
-            <form
+        {
+            formulario ? (
+                <form
                 className="formulario-nuevo-proyecto"
                 onSubmit={onSubmitProyecto}
-            >
-            <input
-                type="text"
-                className="input-text"
-                placeholder="Nombre Proyecto"
-                name="nombre"
-                value={nombre}
-                onChange={onChangeProyecto}
-            />
+                >
+                <input
+                    type="text"
+                    className="input-text"
+                    placeholder="Nombre Proyecto"
+                    name="nombre"
+                    value={nombre}
+                    onChange={onChangeProyecto}
+                />
 
-            <input
-                type="text"
-                className="btn btn-block btn-primario"
-                value="Agregar Proyecto"
-            />
+                <input
+                    type="text"
+                    className="btn btn-block btn-primario"
+                    value="Agregar Proyecto"
+                />
 
-            </form>
+                </form>
+            ) : null
+        }
+
         </Fragment>
     );
 }
