0efbc2270b1686a58f21f7f8c3a88bb34797d3d1
Jimmy <bruno_pier@hotmail.com>
2020-01-27 17:02:46 -0300

    Adding new file&button code - NuevoProyecto.js

---
+++ src/components/proyectos/NuevoProyecto.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NuevoProyecto = () => {
+    return (
+        <button
+            type="button"
+            className="btn btn-block btn-primario"
+        >Nuevo Proyecto</button>
+    );
+}
+
+export default NuevoProyecto;
