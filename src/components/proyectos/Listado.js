11c9dc89499f87753baff49c687505e20b5d2c19
Jimmy <bruno_pier@hotmail.com>
2020-01-27 17:23:41 -0300

    Adding import Proyecto - Listado.js

---
+++ src/components/proyectos/Listado.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Proyecto from './Proyecto';
+
+const ListadoProyectos = () => {
+    return (
+        <ul className="listado-proyectos">
+
+        </ul>
+    );
+}
+
+export default ListadoProyectos;
