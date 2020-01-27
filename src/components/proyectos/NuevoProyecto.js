54dfff30ffe04540579c97466ecde355ef07fbef
Jimmy <bruno_pier@hotmail.com>
2020-01-27 17:08:06 -0300

    Adding Fragment&form code - NuevoProyecto.js

--- src/components/proyectos/NuevoProyecto.js
+++ src/components/proyectos/NuevoProyecto.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const NuevoProyecto = () => {
     return (
-        <button
-            type="button"
-            className="btn btn-block btn-primario"
-        >Nuevo Proyecto</button>
+        <Fragment>
+            <button
+                type="button"
+                className="btn btn-block btn-primario"
+            >Nuevo Proyecto</button>
+
+            <form
+                className="formulario-nuevo-proyecto"
+            >
+            <input
+                type="text"
+                className="input-text"
+                placeholder="Nombre Proyecto"
+                name="nombre"
+            />
+
+            <input
+                type="text"
+                className="btn btn-block btn-primario"
+                value="Agregar Proyecto"
+            />
+
+            </form>
+        </Fragment>
     );
 }
 
