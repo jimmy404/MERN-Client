3dd9afe8b87c3ebdbc2a424a4e7c1ad97fb81c33
Jimmy <bruno_pier@hotmail.com>
2020-01-28 13:17:05 -0300

    Adding initialState in proyectoState.js

--- src/context/proyectos/proyectoState.js
+++ src/context/proyectos/proyectoState.js
@@ -3,8 +3,16 @@ import React, { useReducer } from 'react';
 import proyectoContext from './proyectoContext';
 import proyectoReducer from './proyectoReducer';
 import {  FORMULARIO_PROYECTO } from '../../types';
+
+
 const ProyectoState = props => {
+
     const initialState = {
+        proyectos: [
+            { id: 1, nombre: 'Tienda Virtual' },
+            { id: 2, nombre: 'Intranet' },
+            { id: 3, nombre: 'Shopping' }
+        ],
         formulario: false
     }
 
@@ -21,6 +29,7 @@ const ProyectoState = props => {
     return(
         <proyectoContext.Provider
             value={{
+                proyectos: state.proyectos,
                 formulario: state.formulario,
                 mostrarFormulario
             }}
