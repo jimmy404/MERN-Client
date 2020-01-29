4e1bb15a3b97b6195a89f2b80105cd4b963cfec2
Jimmy <bruno_pier@hotmail.com>
2020-01-28 23:19:40 -0300

    Showing the name of the project - code - ListadoTareas.js

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -8,6 +8,12 @@ const ListadoTareas = () => {
     const proyectosContext = useContext(proyectoContext);
     const { proyecto } = proyectosContext;
 
+    //Si no hay proyecto seleccionado
+    if(!proyecto) return <h2>Selecciona un proyecto</h2>;
+
+    //Array destructuring para extraer el proyecto actual
+    const [proyectoActual] = proyecto;
+
     const tareasProyecto = [
         { id: 1, nombre: 'Elegir Plataforma', estado: true},
         { id: 2, nombre: 'Elegir Codigo', estado: false},
@@ -18,7 +24,7 @@ const ListadoTareas = () => {
 
     return (
         <Fragment>
-        <h2>Proyecto: Tienda Virtual</h2>
+        <h2>Proyecto: {proyectoActual.nombre}</h2>
         <ul className="listado-tareas">
             {tareasProyecto.length === 0
                 ? (<li className="tarea"><p>No hay tareas</p></li>)
