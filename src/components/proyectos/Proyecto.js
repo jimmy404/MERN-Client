0e9ff672c970c5674974e03f717dc4f691b3ea24
Jimmy <bruno_pier@hotmail.com>
2020-01-29 19:48:17 -0300

    Adding seleccionarProyecto code&import - Proyecto.js

--- src/components/proyectos/Proyecto.js
+++ src/components/proyectos/Proyecto.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import proyectoContext from '../../context/proyectos/proyectoContext';
+import tareaContext from '../../context/tareas/tareaContext';
 
 
 const Proyecto = ({proyecto}) => {
@@ -8,13 +9,23 @@ const Proyecto = ({proyecto}) => {
         const proyectosContext = useContext(proyectoContext);
         const { proyectoActual } = proyectosContext;
 
+        //obtener la funcion del context de tarea
+        const tareasContext = useContext(tareaContext);
+        const {obtenerTareas} = tareasContext;
+
+        //funcion para agregar proyecto actual
+        const seleccionarProyecto = id => {
+            proyectoActual(id); // fijar proyecto actual
+            obtenerTareas(id); // filtrar tareas cuando se da click
+        }
+
 
     return (
         <li>
             <button
                 type="button"
                 className="btn btn-blank"
-                onClick={ () => proyectoActual(proyecto.id) }
+                onClick={ () => seleccionarProyecto(proyecto.id) }
             >{proyecto.nombre}</button>
         </li>
     );
