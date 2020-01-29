66c2c5c6b220435316bdf4ea620947c7477d761b
Jimmy <bruno_pier@hotmail.com>
2020-01-29 19:58:00 -0300

    Adding import& code tareasproyecto - ListadoTareas.js

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useContext} from 'react';
 import Tarea from './Tarea';
 import proyectoContext from '../../context/proyectos/proyectoContext';
+import tareaContext from '../../context/tareas/tareaContext';
 
 const ListadoTareas = () => {
 
@@ -8,13 +9,16 @@ const ListadoTareas = () => {
     const proyectosContext = useContext(proyectoContext);
     const { proyecto, eliminarProyecto } = proyectosContext;
 
+    //obtener la funcion del context de tarea
+    const tareasContext = useContext(tareaContext);
+    const {tareasproyecto} = tareasContext;
+
     //Si no hay proyecto seleccionado
     if(!proyecto) return <h2>Selecciona un proyecto</h2>;
 
     //Array destructuring para extraer el proyecto actual
     const [proyectoActual] = proyecto;
 
-    const tareasProyecto = [];
 
     //Eliminar un proyecto
     const onClickEliminar = () => {
@@ -25,9 +29,9 @@ const ListadoTareas = () => {
         <Fragment>
         <h2>Proyecto: {proyectoActual.nombre}</h2>
         <ul className="listado-tareas">
-            {tareasProyecto.length === 0
+            {tareasproyecto.length === 0
                 ? (<li className="tarea"><p>No hay tareas</p></li>)
-                : tareasProyecto.map(tarea => (
+                : tareasproyecto.map(tarea => (
                     <Tarea
                         tarea={tarea}
                         />
