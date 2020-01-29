3ddda950232045303a097f7f6ce9dc5cffd7bc94
Jimmy <bruno_pier@hotmail.com>
2020-01-29 19:46:06 -0300

    Adding new type - index.js

--- src/context/tareas/tareaState.js
+++ src/context/tareas/tareaState.js
@@ -4,14 +4,28 @@ import TareaReducer from './tareaReducer';
 
 const TareaState = props => {
     const initialState = {
-        tareas: [],
+        tareas: [
+            { id: 1, nombre: 'Elegir Plataforma', estado: true, proyectoId: 1},
+            { id: 2, nombre: 'Elegir Codigo', estado: false, proyectoId: 2},
+            { id: 3, nombre: 'Elegir Hosting', estado: false, proyectoId: 3},
+            { id: 1, nombre: 'Elegir Plataforma', estado: true, proyectoId: 1},
+            { id: 2, nombre: 'Elegir Codigo', estado: false, proyectoId: 2},
+            { id: 3, nombre: 'Elegir Hosting', estado: false, proyectoId: 3},
+            { id: 1, nombre: 'Elegir Plataforma', estado: true, proyectoId: 1},
+            { id: 2, nombre: 'Elegir Codigo', estado: false, proyectoId: 2},
+            { id: 3, nombre: 'Elegir Hosting', estado: false, proyectoId: 3},
+            { id: 4, nombre: 'Elegir Forma de Pago', estado: true, proyectoId: 4}
+        ],
     }
 
     //Crear dispatch y state
     const [state, dispatch] = useReducer(TareaReducer, initialState);
 
     return(
-        <TareaContext.Provider>
+        <TareaContext.Provider
+        value={{
+            tareas: state.tareas
+        }}>
             {props.children}
         </TareaContext.Provider>
     )
