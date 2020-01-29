a967cf53efc7d2d05f9a4bbabe7f0b5fc10170ff
Jimmy <bruno_pier@hotmail.com>
2020-01-29 19:47:18 -0300

    Adding seleccionarProyecto code&import - tareaState.js

--- src/context/tareas/tareaState.js
+++ src/context/tareas/tareaState.js
@@ -2,6 +2,10 @@ import React, { useReducer } from 'react';
 import TareaContext from './tareaContext';
 import TareaReducer from './tareaReducer';
 
+import
+        {TAREAS_PROYECTO}
+    from '../../types';
+
 const TareaState = props => {
     const initialState = {
         tareas: [
@@ -21,10 +25,22 @@ const TareaState = props => {
     //Crear dispatch y state
     const [state, dispatch] = useReducer(TareaReducer, initialState);
 
+    //crear funciones
+
+    //obtener las tareas de un proyecto especifico
+    const obtenerTareas = proyectoId => {
+        dispatch({
+            type: TAREAS_PROYECTO,
+            payload: proyectoId,
+            obtenerTareas
+        })
+    }
+
     return(
         <TareaContext.Provider
         value={{
-            tareas: state.tareas
+            tareas: state.tareas,
+            obtenerTareas
         }}>
             {props.children}
         </TareaContext.Provider>
