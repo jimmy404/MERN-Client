3f72e9ef92f9149e51e663f89c68cc025f128e14
Jimmy <bruno_pier@hotmail.com>
2020-01-30 14:17:14 -0300

    Adding import&code&fix bug - to modify the status of a task - tareaState.js

--- src/context/tareas/tareaState.js
+++ src/context/tareas/tareaState.js
@@ -6,7 +6,8 @@ import {
         TAREAS_PROYECTO,
         AGREGAR_TAREA,
         VALIDAR_TAREA,
-        ELIMINAR_TAREA
+        ELIMINAR_TAREA,
+        ESTADO_TAREA
     } from '../../types';
 
 const TareaState = props => {
@@ -63,6 +64,15 @@ const TareaState = props => {
             payload: id
         })
     }
+
+    //cambia el estado de cada tarea
+    const cambiarEstadoTarea = tarea => {
+        dispatch({
+            type: ESTADO_TAREA,
+            payload: tarea
+        })
+    }
+
     return(
         <TareaContext.Provider
         value={{
@@ -72,7 +82,8 @@ const TareaState = props => {
             obtenerTareas,
             agregarTarea,
             validarTarea,
-            eliminarTarea
+            eliminarTarea,
+            cambiarEstadoTarea
         }}>
             {props.children}
         </TareaContext.Provider>
