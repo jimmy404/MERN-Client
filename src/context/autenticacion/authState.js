0bf4157ff62352cdb639f7600879c68814059baf
Jimmy <bruno_pier@hotmail.com>
2020-02-02 13:59:45 -0300

    Adding code in usuarioAutenticado - authState.js

--- src/context/autenticacion/authState.js
+++ src/context/autenticacion/authState.js
@@ -3,6 +3,7 @@ import AuthContext from './authContext';
 import AuthReducer from './authReducer';
 
 import clienteAxios from '../../config/axios';
+import tokenAuth from '../../config/token';
 
 import {
     REGISTRO_EXITOSO,
@@ -55,12 +56,18 @@ const AuthState = props => {
         const token = localStorage.getItem('token');
         if(token){
             //TODO: func para enviar token por headers
+            tokenAuth(token);
         }
 
         try {
             const respuesta = await clienteAxios.get('/api/auth');
-            console.log(respuesta);
+            //console.log(respuesta);
+            dispatch({
+                type: OBTENER_USUARIO,
+                payload: respuesta.data.usuario
+            })
         } catch (error) {
+            console.log(error.response);
             dispatch({
                 type: LOGIN_ERROR
             })
