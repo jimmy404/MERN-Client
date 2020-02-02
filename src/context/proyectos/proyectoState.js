ba4b5f861ac87554858ba4ccff13f34501d6a36b
Jimmy <bruno_pier@hotmail.com>
2020-02-02 19:41:44 -0300

    Adding code in catch> Error message - proyectoState.js

--- src/context/proyectos/proyectoState.js
+++ src/context/proyectos/proyectoState.js
@@ -6,6 +6,7 @@ import {
         FORMULARIO_PROYECTO,
         OBTENER_PROYECTOS,
         AGREGAR_PROYECTO,
+        PROYECTO_ERROR,
         VALIDAR_FORMULARIO,
         PROYECTO_ACTUAL,
         ELIMINAR_PROYECTO
@@ -20,7 +21,8 @@ import clienteAxios from '../../config/axios';
         proyectos: [],
         formulario: false,
         errorformulario: false,
-        proyecto: null
+        proyecto: null,
+        mensaje: null
     }
 
     //Dispatch para ejecutar las acciones
@@ -42,7 +44,14 @@ import clienteAxios from '../../config/axios';
                 payload: resultado.data.proyectos
             })
         } catch (error) {
-            console.log(error);
+            const alerta = {
+                msg: 'Hubo un error',
+                categoria: 'alerta-error'
+            }
+            dispatch({
+                type: PROYECTO_ERROR,
+                payload: alerta
+            })
         }
     }
 
@@ -58,7 +67,14 @@ import clienteAxios from '../../config/axios';
                 payload: resultado.data
             })
         } catch (error) {
-            console.log(error);
+            const alerta = {
+                msg: 'Hubo un error',
+                categoria: 'alerta-error'
+            }
+            dispatch({
+                type: PROYECTO_ERROR,
+                payload: alerta
+            })
         }
     }
 
@@ -86,7 +102,14 @@ import clienteAxios from '../../config/axios';
                 payload: proyectoId
             })
         } catch (error) {
-            console.log(error);
+            const alerta = {
+                msg: 'Hubo un error',
+                categoria: 'alerta-error'
+            }
+            dispatch({
+                type: PROYECTO_ERROR,
+                payload: alerta
+            })
         }
     }
 
@@ -97,6 +120,7 @@ import clienteAxios from '../../config/axios';
                 formulario: state.formulario,
                 errorformulario: state.errorformulario,
                 proyecto: state.proyecto,
+                mensaje: state.mensaje,
                 mostrarFormulario,
                 obtenerProyectos,
                 agregarProyecto,
