75ab8d8c3993803fdb7b75db57524f748906015c
Jimmy <bruno_pier@hotmail.com>
2020-02-02 20:58:47 -0300

    Adding key _id - Tarea.js

--- src/components/tareas/Tarea.js
+++ src/components/tareas/Tarea.js
@@ -15,7 +15,7 @@ const Tarea = ({tarea}) => {
 
     //funcion que se ejecuta cuando el usuario presiona el btn de eliminar tarea
     const tareaEliminar = id => {
-        eliminarTarea(id);
+        eliminarTarea(id, proyectoActual._id);
         obtenerTareas(proyectoActual.id)
     }
 
@@ -72,7 +72,7 @@ const Tarea = ({tarea}) => {
                     <button
                         type="button"
                         className="btn btn-secundario"
-                        onClick={() => tareaEliminar(tarea.id)}
+                        onClick={() => tareaEliminar(tarea._id)}
                     >Eliminar</button>
             </div>
         </li>
