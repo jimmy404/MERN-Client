baaf76f3fe3bb8d2cbd24b3054867d77918eb8fc
Jimmy <bruno_pier@hotmail.com>
2020-02-02 15:50:24 -0300

    Adding ternary code&rules - Barra.js

--- src/components/layout/Barra.js
+++ src/components/layout/Barra.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
+import AuthContext from '../../context/autenticacion/authContext';
+
 
 const Barra = () => {
+
+    //extraer info de autenticacion
+    const authContext = useContext(AuthContext);
+    const { usuario, usuarioAutenticado, cerrarSesion } = authContext;
+
+    useEffect(() => {
+        usuarioAutenticado();
+    }, []);
+
     return (
         <header className="app-header">
-            <p className="nombre-usuario">Hola <span>Jimmy</span></p>
+            {usuario ? <p className="nombre-usuario">Hola <span>{usuario.nombre}</span></p> : null}
             <nav className="nav-principal">
-                <a href="#!">Cerrar Sesion</a>
+                <button
+                    className="btn btn-blank cerrar-sesion"
+                    onClick={() => cerrarSesion()}
+                    >Cerrar Sesion</button>
             </nav>
         </header>
     );
