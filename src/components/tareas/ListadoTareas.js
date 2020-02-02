bbee6335d061863eaaa846384d6676ef755e2887
Jimmy <bruno_pier@hotmail.com>
2020-02-02 20:41:34 -0300

    Adding key _id - ListadoTareas.js

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -35,7 +35,7 @@ const ListadoTareas = () => {
                 : <TransitionGroup>
                     {tareasproyecto.map(tarea => (
                         <CSSTransition
-                            key={tarea.id}
+                            key={tarea._id}
                             timeout={200}
                             classNames="tarea"
                         >
