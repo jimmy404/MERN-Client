4563f0a1e18a0c00df2f4c133090e20cd4b2c745
Jimmy <bruno_pier@hotmail.com>
2020-02-02 19:42:46 -0300

    Adding alerta code message in return - ListadoProyectos.js

--- src/components/proyectos/ListadoProyectos.js
+++ src/components/proyectos/ListadoProyectos.js
@@ -1,19 +1,28 @@
 import React, { useContext, useEffect } from 'react';
 import Proyecto from './Proyecto';
 import proyectoContext from '../../context/proyectos/proyectoContext';
+import AlertaContext from '../../context/alertas/alertaContext';
 import {TransitionGroup, CSSTransition} from 'react-transition-group';
 
 const ListadoProyectos = () => {
 
     //Extraer proyectos del state inicial
     const proyectosContext = useContext(proyectoContext);
-    const { proyectos, obtenerProyectos } = proyectosContext;
+    const { mensaje, proyectos, obtenerProyectos } = proyectosContext;
+
+    const alertaContext = useContext(AlertaContext);
+    const { alerta, mostrarAlerta } = alertaContext;
 
     //obtener proyectos cuando carga el componente
     useEffect(() => {
+
+    //si hay error
+        if(mensaje){
+            mostrarAlerta(mensaje.msg, mensaje.categoria);
+        }
         obtenerProyectos();
         //eslint-disable-next-line
-    }, []);
+    }, [mensaje]);
 
     //revisar si proyectos tiene contenido
     if(proyectos.length === 0 ) return <p>¡No hay proyectos! ¡Empieza a crear!</p>;
@@ -21,6 +30,7 @@ const ListadoProyectos = () => {
 
     return (
         <ul className="listado-proyectos">
+                {alerta ? (<div className={`alerta ${alerta.categoria}`}>{alerta.msg}</div>) : null}
             <TransitionGroup>
             {proyectos.map(proyecto => (
                 <CSSTransition
