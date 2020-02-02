9ca64c04baf905d8cb5e451133007aae596ba59b
Jimmy <bruno_pier@hotmail.com>
2020-02-02 01:43:30 -0300

    Adding console.log test - App.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import AuthState from './context/autenticacion/authState';
 
 
 function App() {
+  console.log(process.env.REACT_APP_BACKEND_URL);
   return (
     <ProyectoState>
       <TareasState>
