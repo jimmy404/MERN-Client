1b9747c6ec0ced77304ecf0d41cda0bcf9f5d433
Jimmy <bruno_pier@hotmail.com>
2020-02-02 14:30:06 -0300

    Adding code in iniciarSesion - authState.js

--- src/context/autenticacion/authState.js
+++ src/context/autenticacion/authState.js
@@ -74,6 +74,32 @@ const AuthState = props => {
         }
     }
 
+    //cuando el usuario inicia sesion
+    const iniciarSesion = async datos => {
+        try {
+            const respuesta = await clienteAxios.post('/api/auth', datos);
+            dispatch({
+                type: LOGIN_EXITOSO,
+                payload: respuesta.data
+            });
+
+            //obtener el usuario
+            usuarioAutenticado();
+
+        } catch (error) {
+            console.log(error.response.data.msg);
+            const alerta={
+                msg: error.response.data.msg,
+                categoria: 'alerta-error'
+            }
+
+            dispatch({
+                type: LOGIN_ERROR,
+                payload: alerta
+            })
+        }
+    }
+
     return(
         <AuthContext.Provider
             value={{
@@ -81,7 +107,8 @@ const AuthState = props => {
                 autenticado: state.autenticado,
                 usuario: state.usuario,
                 mensaje: state.mensaje,
-                registrarUsuario
+                registrarUsuario,
+                iniciarSesion
             }}
         >{props.children}
 
