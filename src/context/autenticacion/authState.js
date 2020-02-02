d41e4a9d47016dc6908b0669d6a47c7985608ce7
Jimmy <bruno_pier@hotmail.com>
2020-02-02 02:09:14 -0300

    Adding registrarUsuario code & adding value in return - authState.js

--- src/context/autenticacion/authState.js
+++ src/context/autenticacion/authState.js
@@ -2,6 +2,7 @@ import React, {useReducer} from 'react';
 import AuthContext from './authContext';
 import AuthReducer from './authReducer';
 
+import clienteAxios from '../../config/axios';
 
 import {
     REGISTRO_EXITOSO,
@@ -23,7 +24,21 @@ const AuthState = props => {
     const [state, dispatch] = useReducer(AuthReducer, initialState);
 
     //Funciones
+    const registrarUsuario = async datos => {
+        try {
+            const respuesta = await clienteAxios.post('/api/usuarios', datos);
+            console.log(respuesta);
+            dispatch({
+                type: REGISTRO_EXITOSO
+            })
+        } catch (error) {
+            console.log(error);
 
+            dispatch({
+                type: REGISTRO_ERROR
+            })
+        }
+    }
 
     return(
         <AuthContext.Provider
@@ -31,7 +46,8 @@ const AuthState = props => {
                 token: state.token,
                 autenticado: state.autenticado,
                 usuario: state.usuario,
-                mensaje: state.mensaje
+                mensaje: state.mensaje,
+                registrarUsuario
             }}
         >{props.children}
 
