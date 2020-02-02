0de5181d0f85d4538e2f588cc8f7e06e70bf18aa
Jimmy <bruno_pier@hotmail.com>
2020-02-02 02:40:40 -0300

    Adding useEffect import&code&rules - NuevaCuenta.js

--- src/components/auth/NuevaCuenta.js
+++ src/components/auth/NuevaCuenta.js
@@ -1,16 +1,27 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import AlertaContext from '../../context/alertas/alertaContext';
 import AuthContext from '../../context/autenticacion/authContext';
 
-const NuevaCuenta = () => {
+const NuevaCuenta = (props) => {
 
     //extraer valores del context
     const alertaContext = useContext(AlertaContext);
     const { alerta, mostrarAlerta} = alertaContext;
 
     const authContext = useContext(AuthContext);
-    const {registrarUsuario} = authContext;
+    const {mensaje, autenticado, registrarUsuario} = authContext;
+
+    //en caso de que el usuario se haya registrado, autenticado o duplique
+    useEffect(() => {
+        if(autenticado) {
+            props.history.push('/proyectos');
+        }
+        if(mensaje) {
+            mostrarAlerta(mensaje.msg, mensaje.categoria);
+        }
+
+    }, [mensaje, autenticado, props.history])
 
     //State login
     const [usuario, guardarUsuario] = useState({
